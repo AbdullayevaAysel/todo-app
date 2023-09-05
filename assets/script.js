ff19dfa5940a3fd76599ea65827e2aff0ae170ce
Aysel <aysel.abdullayevagithub@gmail.com>
2023-09-05 21:04:47 +0400

    version 1.3

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
+  $("body").attr("thema", localStorage.getItem("thema"))
+
   $("#toggle-dark").on("click", function () {
     $("body").attr("thema", (_, attr) => (attr == "dark" ? "light" : "dark"))
+    localStorage.setItem("thema", $("body").attr("thema"))
   })
 
   const calcCount = (el) => {
@@ -10,7 +13,6 @@ $(document).ready(function () {
   function getEl(el) {
     $(el).on("click", function () {
       let li = $(this).closest("li")
-      console.log(li);
       if ($(this).hasClass("compileted-btn")) {
         $(this).removeClass("compileted-btn")
         $(li).removeClass("compileted")
@@ -24,6 +26,7 @@ $(document).ready(function () {
         // notification
         notification($(li).find("span").text(), "compileted oldu", "")
       }
+      updateTodoObject($(li).attr("key"))
     })
   }
 
@@ -33,12 +36,19 @@ $(document).ready(function () {
       calcCount($(".lists"))
 
       notification(val, `todo silindi`, "")
+      removeTodoObject(el.closest("li").attr("key"))
     })
   }
 
+  let count = localStorage.getItem("todos")
+    ? Math.max(
+        ...$.map(JSON.parse(localStorage.getItem("todos")), (item) => item.id)
+      )
+    : 0
+
   const addTodo = (val) => {
     let li = $(`
-    <li class="list-item" draggable="true">
+    <li key=${count + 1} class="list-item" draggable="true">
       <div>
         <button type="button" class="check">
           <img src="./images/icon-check.svg" alt="">
@@ -74,14 +84,16 @@ $(document).ready(function () {
     }, 700)
   }
 
+  let i = 0
   const callAddTodo = (input, e) => {
     e.preventDefault()
     if ($(input).val() !== "") {
       let val = $(input).val()
-      addTodo(val)
+      addTodo(val, i)
       notification(val, `elave olundu`, "+")
       addTodoToObject(val)
       $(input).val("")
+      i++
     }
   }
 
@@ -121,24 +133,31 @@ $(document).ready(function () {
     addActive($(this))
   })
 
-  $('[data-filter="clear-completed"]').on("click", function (e) {
-    let li = $(".lists li.compileted")
-    $(li).remove()
-    notification($(li).find("span").text(), "compiletedler silindi", "")
-    calcCount($(".lists"))
-    addActive($(this))
-  })
+  $('[data-filter="clear-completed"]').on("click", () =>
+    a($(".lists li.compileted"))
+  )
+
+  function a(li) {
+    $(li).each(function () {
+      $(li).remove()
+      notification($(li).find("span").text(), "compiletedler silindi", "")
+      calcCount($(".lists"))
+      addActive($(this))
+      removeTodoObject($(this).attr("key"))
+    })
+  }
 
   // add localStorage
-
   let todos = localStorage.getItem("todos")
     ? JSON.parse(localStorage.getItem("todos"))
     : []
-  let count = 0
+
   function addTodoToObject(val) {
-    let obj = {}
-    obj["id"] = count
-    obj["name"] = val
+    let obj = {
+      id: count + 1,
+      compileted: false,
+      name: val,
+    }
     todos.unshift(obj)
     addTodoToLocalStorage(todos)
     count++
@@ -152,19 +171,54 @@ $(document).ready(function () {
   function showTodoGetLocalStorage() {
     let getData = localStorage.getItem("todos")
     let parseObj = JSON.parse(getData)
-    let html = parseObj.map(
-      (item) =>
-        `<li key=${item?.id} class="list-item" draggable="true">
-      <div>
-        <button type="button" class="check">
-          <img src="./images/icon-check.svg" alt="">
-        </button>
-        <span>${item?.name}</span>
-      </div>
-      <img class="remove" src="./images/icon-cross.svg" alt="">
-    </li>`
+    let html = parseObj?.map((item) =>
+      $(
+        `<li key=${item?.id} class="list-item iii ${
+          item?.compileted && "compileted"
+        }" draggable="true">
+        <div>
+          <button type="button" class="check ${
+            item?.compileted && "compileted-btn"
+          }">
+            <img src="./images/icon-check.svg" alt="">
+          </button>
+          <span>${item?.name}</span>
+        </div>
+        <img class="remove" src="./images/icon-cross.svg" alt="">
+      </li>`
+      )
     )
+    $(html).each(function () {
+      getEl($(this).find(".check"))
+      removeEl($(this).find(".remove"), $(this).find("span").val())
+      $('[data-filter="clear-completed"]').on(
+        "click",
+        () => $(this).hasClass(".compileted") && a($(this).find(".compileted"))
+      )
+    })
     $(".lists").append(html)
+    calcCount($(".lists"))
   }
   showTodoGetLocalStorage()
+
+  // Todo check from localStorage
+  function updateTodoObject(id) {
+    let clonedTodos = JSON.parse(localStorage.getItem("todos"))
+    let newTodo = clonedTodos?.map((todo) => {
+      if (todo?.id == id) {
+        return { ...todo, compileted: !todo?.compileted }
+      }
+      return todo
+    })
+    localStorage.setItem("todos", JSON.stringify(newTodo))
+    clonedTodos = newTodo
+  }
+
+  // remove todo from localStorage
+  function removeTodoObject(id) {
+    let clonedTodos = JSON.parse(localStorage.getItem("todos"))
+    let newTodo = clonedTodos?.filter((todo) => todo.id != id)
+    localStorage.setItem("todos", JSON.stringify(newTodo))
+    clonedTodos = newTodo
+  }
 })
